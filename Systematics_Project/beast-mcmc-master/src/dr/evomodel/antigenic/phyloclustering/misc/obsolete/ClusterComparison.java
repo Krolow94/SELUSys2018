954906a850cb3bd12915b6bd03376bd4e550a542
Aaron Krolow <aaron.krolow@selu.edu>
2018-05-12 15:22:08 -0500

    this hopefully works

--- Systematics_Project/beast-mcmc-master/src/dr/evomodel/antigenic/phyloclustering/misc/obsolete/ClusterComparison.java
+++
@@ -1,8 +0,0 @@
-package dr.evomodel.antigenic.phyloclustering.misc.obsolete;
-
-public class ClusterComparison {
- public int minNumMismatchPerfectSeparation;
- public int sampleSize;
- public double SS;
- public double scaledMinNumMismatchPerfectSeparation;
-}
